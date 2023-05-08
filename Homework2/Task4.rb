650d9afe1f12f1f0473ef73f26eee42d360fa47b
uncleserge <serge@sadowsky.spb.ru>
2023-05-08 20:31:36 +0300

    Finished Hw2 task 4

---
+++ Homework2/Task4.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
+
+letters = %w[a e i o u y]
+letter_numbers = {}
+letters.each { |item| letter_numbers[item] = item.ord-96}
+puts letter_numbers
+
